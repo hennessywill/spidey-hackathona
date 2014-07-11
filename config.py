92334668cd207d6ff431ac40b8f8579a533ed228
hellovai <beagle@hellovai.com>
2014-07-11 00:33:16 +0000

    adding new file to config

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ from state import State
 from state import StateConfig
 
 hip = {
-  "pins":["P8_13"],
+  "pins":["P8_13", "P8_34", "P9_21", "P9_22"],
   "state_config": StateConfig([0, 130, 180], 0),
 }
 
 knee = {
-  "pins":[],
-  "state_config": StateConfig([]),
+  "pins":["P8_45", "P8_46", "P9_14", "P9_29"],
+  "state_config": StateConfig([0, 180]),
 }
 
 override = {
